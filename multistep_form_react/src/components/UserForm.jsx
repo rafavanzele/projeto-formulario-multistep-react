7359d8eff49ece2a0d53a4b3f7bfabb98e90e7d1
rafavanzele <rafa_vanzele@hotmail.com>
2024-06-13 16:52:25 -0300

    css inicial

--- multistep_form_react/src/components/UserForm.jsx
+++ multistep_form_react/src/components/UserForm.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const UserForm = () => {
   return (
     <div>
-        <h2>User Form</h2>
+        <div className="form-control">
+          <label htmlFor="name">Nome: </label>
+          <input type="text" name='name' id='name' placeholder='Digite seu nome' required />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="email">Email: </label>
+          <input type="email" name='email' id='email' placeholder='Digite seu email' required />
+        </div>
     </div>
   )
 }
