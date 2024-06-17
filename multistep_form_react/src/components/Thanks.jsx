e0d63d058e6fab68cc228561caf249fa5098a4bc
rafavanzele <rafa_vanzele@hotmail.com>
2024-06-17 17:34:13 -0300

    componente thanks + css

--- multistep_form_react/src/components/Thanks.jsx
+++ multistep_form_react/src/components/Thanks.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+// Importando emojis
+import {BsFillEmojiHeartEyesFill, BsEmojiSmileFill, BsEmojiNeutralFill, BsEmojiFrownFill, BsEmojiHeartEyesFill} from 'react-icons/bs'
+
+// Estilos
+import './Thanks.css'
 
 const Thanks = () => {
   return (
-    <div>
-        <h2>Thanks</h2>
+    <div className='thanks-container'>
+        <h2>Falta pouco...</h2>
+        <p>A sua opinião é muito importante. Em breve você recebrá um cupom de desconto para sua próxima compra</p>
+        <p>Para concluir sua avaliação, clique no botão de Enviar abaixo</p>
+
+        <h3>Aqui está o resumo da sua avaliação: </h3>
+        <p className="review-data">
+          <span>Satisfação com o produto: </span>
+        </p>
+
+        <p className="review-data">
+          <span>Comentário: </span>
+        </p>
     </div>
   )
 }
