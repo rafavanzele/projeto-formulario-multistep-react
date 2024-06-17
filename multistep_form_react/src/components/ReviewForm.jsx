fca73bf108913f6a4cb813351fb1613e70ef36da
rafavanzele <rafa_vanzele@hotmail.com>
2024-06-17 17:20:21 -0300

    componente review + css

--- multistep_form_react/src/components/ReviewForm.jsx
+++ multistep_form_react/src/components/ReviewForm.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+// Importando emojis de avaliação
+import {BsFillEmojiHeartEyesFill, BsEmojiSmileFill, BsEmojiNeutralFill, BsEmojiFrownFill, BsEmojiHeartEyesFill} from 'react-icons/bs'
+
+// ESTILOS
+import './ReviewForm.css'
 
 const ReviewForm = () => {
   return (
-    <div>
-        <h2>Review Form</h2>
+    <div className='review-form'>
+      
+      <div className="form-control score-container">
+        <label className="radio-container">
+          <input type="radio" value='unsatisfied' name='review' required />
+          <BsEmojiFrownFill/>
+          <p>Insatisfeito</p>
+        </label>
+
+        <label className="radio-container">
+          <input type="radio" value='neutral' name='review' required />
+          <BsEmojiNeutralFill/>
+          <p>Poderia ser melhor</p>
+        </label>
+
+        <label className="radio-container">
+          <input type="radio" value='satisfied' name='review' required />
+          <BsEmojiSmileFill/>
+          <p>Satisfeito</p>
+        </label>
+
+        <label className="radio-container">
+          <input type="radio" value='very_satisfied' name='review' required />
+          <BsEmojiHeartEyesFill/>
+          <p>Muito satisfeito</p>
+        </label>
+      </div>
+
+      
+      <div className="form-control">
+        <label htmlFor="comment">Comentário: </label>
+        <textarea name="comment" id="comment" placeholder='Conte como foi sua experiência com nosso produto' required></textarea>
+      </div>
     </div>
   )
 }
